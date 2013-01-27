5669fc60941dedbf1919f63335929a15722510dd
crazy2be <crazy1be@gmail.com>
2013-01-27 14:53:01 -0500

    Basic world/chunk system in place.

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -5,20 +5,13 @@
 
     var mesh;
 
-    var worldWidth = 128, worldDepth = 128,
-    worldHalfWidth = worldWidth / 2, worldHalfDepth = worldDepth / 2,
-    data = generateHeight(worldWidth, worldDepth);
-
     var clock = new THREE.Clock();
-
-    init();
-    animate();
 
     function init() {
         container = document.getElementById('container');
         
         camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.01, 200);
-        camera.position.y = getY(worldHalfWidth, worldHalfDepth) + 1;
+        camera.position.y = getWorldY(0, 0) + 2;
         
         controls = new THREE.FirstPersonControls(camera, container);
         
@@ -30,93 +23,7 @@
         
         scene = new THREE.Scene();
         
-        // sides
-        
-        var matrix = new THREE.Matrix4();
-        
-        var pxGeometry = new THREE.PlaneGeometry(1, 1);
-        pxGeometry.faces[0].materialIndex = 1;
-        pxGeometry.applyMatrix(matrix.makeRotationY(Math.PI / 2));
-        pxGeometry.applyMatrix(matrix.makeTranslation(0.5, 0, 0));
-        
-        var nxGeometry = new THREE.PlaneGeometry(1, 1);
-        nxGeometry.faces[0].materialIndex = 1;
-        nxGeometry.applyMatrix(matrix.makeRotationY(-Math.PI/2));
-        nxGeometry.applyMatrix(matrix.makeTranslation(-0.5, 0, 0));
-        
-        var pyGeometry = new THREE.PlaneGeometry(1, 1);
-        pyGeometry.faces[0].materialIndex = 0;
-        pyGeometry.applyMatrix(matrix.makeRotationX(-Math.PI/2));
-        pyGeometry.applyMatrix(matrix.makeTranslation(0, 0.5, 0));
-        
-        var pzGeometry = new THREE.PlaneGeometry(1, 1);
-        pzGeometry.faces[0].materialIndex = 1;
-        pzGeometry.applyMatrix(matrix.makeTranslation(0, 0, 0.5));
-        
-        var nzGeometry = new THREE.PlaneGeometry(1, 1);
-        nzGeometry.faces[0].materialIndex = 1;
-        nzGeometry.applyMatrix(matrix.makeRotationY(Math.PI));
-        nzGeometry.applyMatrix(matrix.makeTranslation(0, 0, -0.5));
-        
-        var geometry = new THREE.Geometry();
-        var dummy = new THREE.Mesh();
-        
-        for (var z = 0; z < worldDepth; z++) {
-            for (var x = 0; x < worldWidth; x++) {
-                var h = getY(x, z);
-                
-                dummy.position.x = x - worldHalfWidth;
-                dummy.position.y = h;
-                dummy.position.z = z - worldHalfDepth;
-                
-                var px = getY(x + 1, z);
-                var nx = getY(x - 1, z);
-                var pz = getY(x, z + 1);
-                var nz = getY(x, z - 1);
-                
-                dummy.geometry = pyGeometry;
-                THREE.GeometryUtils.merge(geometry, dummy);
-                
-                if ((px != h && px != h + 1) || x == 0) {
-                    dummy.geometry = pxGeometry;
-                    THREE.GeometryUtils.merge(geometry, dummy);
-                }
-                
-                if ((nx != h && nx != h + 1) || x == worldWidth - 1) {
-                    dummy.geometry = nxGeometry;
-                    THREE.GeometryUtils.merge(geometry, dummy);
-                }
-                
-                if ((pz != h && pz != h + 1) || z == worldDepth - 1) {
-                    dummy.geometry = pzGeometry;
-                    THREE.GeometryUtils.merge(geometry, dummy);
-                }
-                
-                if ((nz != h && nz != h + 1) || z == 0) {
-                    dummy.geometry = nzGeometry;
-                    THREE.GeometryUtils.merge(geometry, dummy);
-                }
-            }
-        }
-        
-        var textureGrass = THREE.ImageUtils.loadTexture('img/minecraft/grass.png');
-        textureGrass.magFilter = THREE.NearestFilter;
-        textureGrass.minFilter = THREE.LinearMipMapLinearFilter;
-        
-        var textureGrassDirt = THREE.ImageUtils.loadTexture('img/minecraft/grass_dirt.png');
-        textureGrassDirt.magFilter = THREE.NearestFilter;
-        textureGrassDirt.minFilter = THREE.LinearMipMapLinearFilter;
-        
-        var material1 = new THREE.MeshLambertMaterial({
-            map: textureGrass,
-            ambient: 0xbbbbbb
-        });
-        var material2 = new THREE.MeshLambertMaterial({
-            map: textureGrassDirt,
-            ambient: 0xbbbbbb
-        });
-        
-        var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial([material1, material2]));
+		var mesh = world.generateMesh();
         scene.add(mesh);
         
         var ambientLight = new THREE.AmbientLight(0xcccccc);
@@ -173,15 +80,14 @@
         }
         return data;
     }
-
-    function getY(x, z) {
-        return (data[x + z*worldWidth] * 0.2) | 0;
-    }
+    
+    var data = generateHeight(64, 64);
+	var world = new World(data);
     
     function getWorldY(x, z) {
-        var gx = (x + worldHalfWidth) | 0;
-        var gz = (z + worldHalfDepth) | 0;
-        var gy = getY(gx, gz);
+        var gx = (x + 32) | 0;
+        var gz = (z + 32) | 0;
+        var gy = world.y(gx, gz);
         return gy;
     }
 
@@ -208,4 +114,7 @@
         renderer.render(scene, camera);
         stats.update();
     }
+    
+    init();
+	animate();
 }());
--- src/client/js/world.js
+++ src/client/js/world.js
@@ -1,33 +1,126 @@
-function Chunck() {
+var Chunk = (function () {
+	var matrix = new THREE.Matrix4();
 	
-}
+	var pxGeometry = new THREE.PlaneGeometry(1, 1);
+	pxGeometry.faces[0].materialIndex = 1;
+	pxGeometry.applyMatrix(matrix.makeRotationY(Math.PI / 2));
+	pxGeometry.applyMatrix(matrix.makeTranslation(0.5, 0, 0));
+	
+	var nxGeometry = new THREE.PlaneGeometry(1, 1);
+	nxGeometry.faces[0].materialIndex = 1;
+	nxGeometry.applyMatrix(matrix.makeRotationY(-Math.PI/2));
+	nxGeometry.applyMatrix(matrix.makeTranslation(-0.5, 0, 0));
+	
+	var pyGeometry = new THREE.PlaneGeometry(1, 1);
+	pyGeometry.faces[0].materialIndex = 0;
+	pyGeometry.applyMatrix(matrix.makeRotationX(-Math.PI/2));
+	pyGeometry.applyMatrix(matrix.makeTranslation(0, 0.5, 0));
+	
+	var pzGeometry = new THREE.PlaneGeometry(1, 1);
+	pzGeometry.faces[0].materialIndex = 1;
+	pzGeometry.applyMatrix(matrix.makeTranslation(0, 0, 0.5));
+	
+	var nzGeometry = new THREE.PlaneGeometry(1, 1);
+	nzGeometry.faces[0].materialIndex = 1;
+	nzGeometry.applyMatrix(matrix.makeRotationY(Math.PI));
+	nzGeometry.applyMatrix(matrix.makeTranslation(0, 0, -0.5));
+	
+	var CHUNK_WIDTH = 64;
+	var CHUNK_DEPTH = 64;
+	
+	return Chunk;
+	
+	function Chunk(data) {
+		var cx = -CHUNK_DEPTH / 2;
+		var cz = -CHUNK_WIDTH / 2;
+		var self = this;
 
-function World() {
-    chunks = [];
-	chunks[0] = [];
-	chunks[0][0] = generateChunk(128, 128);
-	chunks[0][1] = generateChunk(128, 128);
+		function getY(x, z) {
+			return (data[x + z*CHUNK_WIDTH] * 0.2) | 0;
+		}
+		
+		function addBlockGeometry(geometry, dummy, x, z) {
+			var h = getY(x, z);
+			
+			dummy.position.x = x + cx;
+			dummy.position.y = h;
+			dummy.position.z = z + cz;
+			
+			var px = getY(x + 1, z);
+			var nx = getY(x - 1, z);
+			var pz = getY(x, z + 1);
+			var nz = getY(x, z - 1);
+			
+			dummy.geometry = pyGeometry;
+			THREE.GeometryUtils.merge(geometry, dummy);
+			
+			if ((px != h && px != h + 1) || x == 0) {
+				dummy.geometry = pxGeometry;
+				THREE.GeometryUtils.merge(geometry, dummy);
+			}
+			
+			if ((nx != h && nx != h + 1) || x == CHUNK_WIDTH - 1) {
+				dummy.geometry = nxGeometry;
+				THREE.GeometryUtils.merge(geometry, dummy);
+			}
+			
+			if ((pz != h && pz != h + 1) || z == CHUNK_DEPTH - 1) {
+				dummy.geometry = pzGeometry;
+				THREE.GeometryUtils.merge(geometry, dummy);
+			}
+			
+			if ((nz != h && nz != h + 1) || z == 0) {
+				dummy.geometry = nzGeometry;
+				THREE.GeometryUtils.merge(geometry, dummy);
+			}	
+		}
+		
+		self.createGeometry = function () {
+			var geometry = new THREE.Geometry();
+			var dummy = new THREE.Mesh();
+			
+			for (var x = 0; x < CHUNK_WIDTH; x++) {
+				for (var z = 0; z < CHUNK_DEPTH; z++) {
+					addBlockGeometry(geometry, dummy, x, z);
+				}
+			}
+			return geometry;
+		}
+		
+		self.y = function (x, z) {
+			return getY(x, z);
+		}
+	}
+}());
 
-    function generateChunk(width, height) {
-        var data = [];
-        var perlin = new ImprovedNoise();
-        var size = width * height;
-        var quality = 2;
-        var z = Math.random() * 100;
-        
-        for (var i = 0; i < size; i++) {
-            data[i] = 0;
-        }
-        
-        for (var i = 0; i < 4; i++) {
-            for (var j = 0; j < size; j++) {
-                var x = j % width
-                var y = (j / width) | 0;
-                
-                data[j] += perlin.noise(x / quality, y / quality, z) * quality;
-            }
-            quality *= 4
-        }
-        return data;
-    }
+function World(data) {
+	var self = this;
+	var chunk = new Chunk(data);
+	var geometry = chunk.createGeometry();
+		
+	var textureGrass = THREE.ImageUtils.loadTexture('img/minecraft/grass.png');
+	textureGrass.magFilter = THREE.NearestFilter;
+	textureGrass.minFilter = THREE.LinearMipMapLinearFilter;
+	
+	var textureGrassDirt = THREE.ImageUtils.loadTexture('img/minecraft/grass_dirt.png');
+	textureGrassDirt.magFilter = THREE.NearestFilter;
+	textureGrassDirt.minFilter = THREE.LinearMipMapLinearFilter;
+	
+	var material1 = new THREE.MeshLambertMaterial({
+		map: textureGrass,
+		ambient: 0xbbbbbb
+	});
+	var material2 = new THREE.MeshLambertMaterial({
+		map: textureGrassDirt,
+		ambient: 0xbbbbbb
+	});
+	
+	self.generateMesh = function () {
+		var mesh = new THREE.Mesh(geometry, new THREE.MeshFaceMaterial([material1, material2]));
+		return mesh;
+	}
+	
+	self.y = function (x, z) {
+		return chunk.y(x, z);
+	}
 }
