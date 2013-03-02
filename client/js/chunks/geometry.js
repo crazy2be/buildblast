208c34d54004fcf8801a1c375a1a25143c23353e
crazy2be <crazy1be@gmail.com>
2013-03-02 13:03:23 -0500

    Cleanup in preperation for far chunks.

--- client/js/chunks/geometry.js
+++ client/js/chunks/geometry.js
@@ -19,152 +19,9 @@ function ChunkGeometry(cc, blocks, manager) {
     // Neighbouring chunks
     var nxc, pxc, nyc, pyc, nzc, pzc;
 
-    function block(ox, oy, oz) {
-        if (validChunkOffset(ox, oy, oz)) {
-            return blocks[ox*cw*ch + oy*cw + oz];
-        }
-        return null;
-    }
-
-    function setBlock (ox, oy, oz, type) {
-        if (validChunkOffset(ox, oy, oz)) {
-            blocks[ox*cw*ch + oy*cw + oz] = type;
-        } else {
-            throw "Invalid offset coords!";
-        }
-    }
-
-    function t(b) {
-        // Transparent
-        return b === 0x1;
-    }
-
-    function addBlockGeometry(verts, index, color, ox, oy, oz) {
-        if (t(block(ox, oy, oz))) return;
-
-        var pxb = block(ox + 1, oy, oz), px;
-        if (pxb) px = t(pxb);
-        else if (pxc) px = t(pxc.block(0, oy, oz));
-        else px = false;
-
-        var nxb = block(ox - 1, oy, oz), nx;
-        if (nxb) nx = t(nxb);
-        else if (nxc) nx = t(nxc.block(cw - 1, oy, oz));
-        else nx = false;
-
-        var pyb = block(ox, oy + 1, oz), py;
-        if (pyb) py = t(pyb);
-        else if (pyc) py = t(pyc.block(ox, 0, oz));
-        else py = false;
-
-        var nyb = block(ox, oy - 1, oz), ny;
-        if (nyb) ny = t(nyb);
-        else if (nyc) ny = t(nyc.block(ox, ch - 1, oz));
-        else ny = false;
-
-        var pzb = block(ox, oy, oz + 1), pz;
-        if (pzb) pz = t(pzb);
-        else if (pzc) pz = t(pzc.block(ox, oy, 0));
-        else ny = false;
-
-        var nzb = block(ox, oy, oz - 1), nz;
-        if (nzb) nz = t(nzb);
-        else if (nzc) nz = t(nzc.block(ox, oy, cd - 1));
-        else nz = false;
-
-        var wx = ox + cx*cw;
-        var wy = oy + cy*ch;
-        var wz = oz + cz*cd;
-
-        function noiseFunc(x, y, z, q) {
-            var val = 0;
-            if (q === undefined) q = 2;
-            if (q > 32) return 0;
-            val += noiseFunc(x * q, y * q, z * q, q * 4) / q;
-            x += x < 0 ? -0.1 : 0.1;
-            y += y < 0 ? -0.1 : 0.1;
-            z += z < 0 ? -0.1 : 0.1;
-            val += perlinNoise(x, y, z);
-            return Math.abs(val) * 2 + 0.2;
-        }
-        var noise = [];
-        function v(x, y, z) {
-            verts.push(x, y, z);
-            noise.push(noiseFunc(x, y, z));
-        }
-
-        function f(mat, normal) {
-            var l = verts.length / 3;
-            // Each face is made up of two triangles
-            index.push(l-4, l-3, l-2);
-            index.push(l-4, l-2, l-1);
-
-            // Dirt color from http://www.colourlovers.com/color/784800/dirt
-            var c = [120/255, 72/255, 0];
-            if (mat === 2) {
-                c = [0, 1, 0];
-            }
-
-            var r = noise.shift();
-            color.push(c[0]*r, c[1]*r, c[2]*r);
-            r = noise.shift();
-            color.push(c[0]*r, c[1]*r, c[2]*r);
-            r = noise.shift();
-            color.push(c[0]*r, c[1]*r, c[2]*r);
-            r = noise.shift();
-            color.push(c[0]*r, c[1]*r, c[2]*r);
-        }
-        if (px) {
-            v(wx + 1, wy    , wz    );
-            v(wx + 1, wy + 1, wz    );
-            v(wx + 1, wy + 1, wz + 1);
-            v(wx + 1, wy    , wz + 1);
-            f(0);
-        }
-        if (nx) {
-            v(wx, wy    , wz + 1);
-            v(wx, wy + 1, wz + 1);
-            v(wx, wy + 1, wz    );
-            v(wx, wy    , wz    );
-            f(1);
-        }
-        if (py) {
-            v(wx    , wy + 1, wz + 1);
-            v(wx + 1, wy + 1, wz + 1);
-            v(wx + 1, wy + 1, wz    );
-            v(wx    , wy + 1, wz    );
-            f(2);
-        }
-        if (ny) {
-            v(wx    , wy, wz    );
-            v(wx + 1, wy, wz    );
-            v(wx + 1, wy, wz + 1);
-            v(wx    , wy, wz + 1);
-            f(3);
-        }
-        if (pz) {
-            v(wx    , wy    , wz + 1);
-            v(wx + 1, wy    , wz + 1);
-            v(wx + 1, wy + 1, wz + 1);
-            v(wx    , wy + 1, wz + 1);
-            f(4);
-        }
-        if (nz) {
-            v(wx    , wy + 1, wz);
-            v(wx + 1, wy + 1, wz);
-            v(wx + 1, wy    , wz);
-            v(wx    , wy    , wz);
-            f(5);
-        }
-    }
-
-    function updateNeighbours() {
-        pxc = manager.chunkAt(cx + 1, cy, cz);
-        nxc = manager.chunkAt(cx - 1, cy, cz);
-        pyc = manager.chunkAt(cx, cy + 1, cz);
-        nyc = manager.chunkAt(cx, cy - 1, cz);
-        pzc = manager.chunkAt(cx, cy, cz + 1);
-        nzc = manager.chunkAt(cx, cy, cz - 1);
+    var precision = 1;
+    self.setPrecision = function (newPrecision) {
+        precision = newPrecision;
     }
 
     self.calculateGeometry = function () {
@@ -230,6 +87,145 @@ function ChunkGeometry(cc, blocks, manager) {
         };
     }
 
-    self.block = block;
-    self.setBlock = setBlock;
+    self.block = function block(ox, oy, oz) {
+        if (validChunkOffset(ox, oy, oz)) {
+            return blocks[ox*cw*ch + oy*cw + oz];
+        }
+        return null;
+    };
+
+    self.setBlock = function setBlock (ox, oy, oz, type) {
+        if (validChunkOffset(ox, oy, oz)) {
+            blocks[ox*cw*ch + oy*cw + oz] = type;
+        } else {
+            throw "Invalid offset coords!";
+        }
+    };
+
+    function t(bl) {
+        // Transparent
+        return bl === 0x1;
+    }
+
+    function b(ox, oy, oz) {
+        if (ox < 0) return nxc ? nxc.block(ox + cw, oy, oz) : null;
+        if (ox >= cw) return pxc ? pxc.block(ox - cw, oy, oz) : null;
+        if (oy < 0) return nyc ? nyc.block(ox, oy + ch, oz) : null;
+        if (oy >= ch) return pyc ? pyc.block(ox, oy - ch, oz) : null;
+        if (oz < 0) return nzc ? nzc.block(ox, oy, oz + cd) : null;
+        if (oz >= cd) return pzc ? pzc.block(ox, oy, oz - cd) : null;
+        return blocks[ox*cw*ch + oy*cw + oz];
+    }
+
+    function tb(ox, oy, oz) {
+        var bl = b(ox, oy, oz);
+        if (bl) return t(bl);
+        else return false;
+    }
+
+    var noise = [];
+    function addBlockGeometry(verts, index, color, ox, oy, oz) {
+        if (tb(ox, oy, oz)) return;
+
+        var px = tb(ox + 1, oy, oz);
+        var nx = tb(ox - 1, oy, oz);
+        var py = tb(ox, oy + 1, oz);
+        var ny = tb(ox, oy - 1, oz);
+        var pz = tb(ox, oy, oz + 1);
+        var nz = tb(ox, oy, oz - 1);
+
+        var wx = ox + cx*cw;
+        var wy = oy + cy*ch;
+        var wz = oz + cz*cd;
+
+        if (px) {
+            v(wx + 1, wy    , wz    );
+            v(wx + 1, wy + 1, wz    );
+            v(wx + 1, wy + 1, wz + 1);
+            v(wx + 1, wy    , wz + 1);
+            f(0);
+        }
+        if (nx) {
+            v(wx, wy    , wz + 1);
+            v(wx, wy + 1, wz + 1);
+            v(wx, wy + 1, wz    );
+            v(wx, wy    , wz    );
+            f(1);
+        }
+        if (py) {
+            v(wx    , wy + 1, wz + 1);
+            v(wx + 1, wy + 1, wz + 1);
+            v(wx + 1, wy + 1, wz    );
+            v(wx    , wy + 1, wz    );
+            f(2);
+        }
+        if (ny) {
+            v(wx    , wy, wz    );
+            v(wx + 1, wy, wz    );
+            v(wx + 1, wy, wz + 1);
+            v(wx    , wy, wz + 1);
+            f(3);
+        }
+        if (pz) {
+            v(wx    , wy    , wz + 1);
+            v(wx + 1, wy    , wz + 1);
+            v(wx + 1, wy + 1, wz + 1);
+            v(wx    , wy + 1, wz + 1);
+            f(4);
+        }
+        if (nz) {
+            v(wx    , wy + 1, wz);
+            v(wx + 1, wy + 1, wz);
+            v(wx + 1, wy    , wz);
+            v(wx    , wy    , wz);
+            f(5);
+        }
+        return;
+
+        function noiseFunc(x, y, z, q) {
+            var val = 0;
+            if (q === undefined) q = 2;
+            if (q > 32) return 0;
+            val += noiseFunc(x * q, y * q, z * q, q * 4) / q;
+            x += x < 0 ? -0.1 : 0.1;
+            y += y < 0 ? -0.1 : 0.1;
+            z += z < 0 ? -0.1 : 0.1;
+            val += perlinNoise(x, y, z);
+            return Math.abs(val) * 2 + 0.2;
+        }
+        function v(x, y, z) {
+            verts.push(x, y, z);
+            noise.push(noiseFunc(x, y, z));
+        }
+        function f(mat, normal) {
+            var l = verts.length / 3;
+            // Each face is made up of two triangles
+            index.push(l-4, l-3, l-2);
+            index.push(l-4, l-2, l-1);
+
+            // Dirt color from http://www.colourlovers.com/color/784800/dirt
+            var c = [120/255, 72/255, 0];
+            if (mat === 2) {
+                c = [0, 1, 0];
+            }
+
+            var r = noise.shift();
+            color.push(c[0]*r, c[1]*r, c[2]*r);
+            r = noise.shift();
+            color.push(c[0]*r, c[1]*r, c[2]*r);
+            r = noise.shift();
+            color.push(c[0]*r, c[1]*r, c[2]*r);
+            r = noise.shift();
+            color.push(c[0]*r, c[1]*r, c[2]*r);
+        }
+    }
+
+    function updateNeighbours() {
+        pxc = manager.chunkAt(cx + 1, cy, cz);
+        nxc = manager.chunkAt(cx - 1, cy, cz);
+        pyc = manager.chunkAt(cx, cy + 1, cz);
+        nyc = manager.chunkAt(cx, cy - 1, cz);
+        pzc = manager.chunkAt(cx, cy, cz + 1);
+        nzc = manager.chunkAt(cx, cy, cz - 1);
+    }
 }
