3d04c3580f1d9dd86b01cfe93f83fe60dbf530ad
Kevin Nash <kieve10@hotmail.com>
2013-04-02 23:36:32 -0400

    Implemented chat message sorting.

--- client/js/chat.js
+++ client/js/chat.js
@@ -1,14 +1,14 @@
 function Chat(controls, conn, container) {
     self = this;
 
-    conn.on('chat', processChat);
-
     var enterPressed = false;
 
     var chatVisibleTime = 3; // In seconds
     var chatFadeTime = 3;    // In seconds
     var chatOpacity = 0;
     var currentTime = 0;
+
+    conn.on('chat', processChat);
 
     self.update = function (dt) {
         if (controls.sample().chat) {
@@ -56,11 +56,24 @@ function Chat(controls, conn, container) {
     });
 
     function processChat(payload) {
-        $("#chat-messages").append("<div class='chat-message-wrapper'><div class='chat-message'>"
+        var $message = $("<div class='chat-message-wrapper'><div class='chat-message'>"
                 + payload.ID + ": " + payload.Message
                 + "</div></div>");
+        $message.attr("time", payload.Time);
+        $("#chat-messages").append($message);
+        var elements = $("#chat-messages").children('div').get();
+        elements.sort(sortChat);
+        $("#chat-messages").empty();
+        $.each(elements, function(i, elm) { $("#chat-messages").append(elm); });
         $("#chat-messages").scrollTop($("#chat-messages")[0].scrollHeight);
         chatOpacity = 1;
         currentTime = chatVisibleTime;
+    }
+
+    function sortChat(a, b) {
+        var aTime = $(a).attr("time");
+        var bTime = $(b).attr("time");
+        console.log("hello", aTime, bTime);
+        return aTime > bTime ? 1 : -1;
     }
 }
