a948318790de2a5dfa0909d1956336ae5b54bd9f
Kevin Nash <kieve10@hotmail.com>
2013-04-11 18:13:12 -0400

    Player quit / join announcements

--- server/src/buildblast/server/world.go
+++ server/src/buildblast/server/world.go
@@ -120,7 +120,7 @@ func (w *World) join(c *Client) {
 	p := NewPlayer()
 	w.players = append(w.players, p)
 	w.clients = append(w.clients, c)
-	log.Println("New player connected! Name: ", c.name)
+	w.announce(c.displayName + " has joined the game!")
 }
 
 func (w *World) leave(c *Client) {
@@ -132,7 +132,7 @@ func (w *World) leave(c *Client) {
 	w.players[i] = w.players[len(w.players)-1]
 	w.players = w.players[0:len(w.players)-1]
 
-	log.Println("Client disconnected...", c.name)
+	w.announce(c.displayName + " has left the game :(")
 
 	m := &MsgEntityRemove{
 		ID: c.name,
