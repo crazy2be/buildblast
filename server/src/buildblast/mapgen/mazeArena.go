176b2119aba49198d034756f2ff456501617f6be
Kevin Nash <kieve10@hotmail.com>
2013-05-04 14:18:53 -0400

    Added random spawns (on the walls)

--- server/src/buildblast/mapgen/mazeArena.go
+++ server/src/buildblast/mapgen/mazeArena.go
@@ -7,13 +7,16 @@ import (
 	"buildblast/coords"
 )
 type MazeArena struct {
-	rand *rand.Rand
-	seed float64
+	seed           float64
+	spawnPointKeys []uint32
+	spawnPoints    map[uint32]coords.World
 }
 
 func NewMazeArena(seed float64) *MazeArena {
 	fa := new(MazeArena)
 	fa.seed = seed
+	fa.spawnPointKeys = make([]uint32, 0)
+	fa.spawnPoints = make(map[uint32]coords.World)
 	return fa
 }
 
@@ -26,6 +29,23 @@ func (fa *MazeArena) Block(wc coords.World) Block {
 
 	val := PerlinNoise(wc.X / 16, wc.Z / 16, fa.seed)
 	if wc.Y < 20 && val - math.Floor(val) < 0.05 {
+		// Don't generate spawns next to walls
+		if (wc.X >= 31 || wc.X <= -31 ||
+			wc.Z >= 127 || wc.Z <= -127) {
+			return BLOCK_STONE
+		}
+
+		possibleSpawn := coords.World{
+			X: wc.X,
+			Y: 21,
+			Z: wc.Z,
+		}
+		hash := possibleSpawn.Hash()
+		_, contained := fa.spawnPoints[hash]
+		if !contained {
+			fa.spawnPointKeys = append(fa.spawnPointKeys, hash)
+			fa.spawnPoints[hash] = possibleSpawn
+		}
 		return BLOCK_STONE
 	}
 
@@ -34,4 +54,18 @@ func (fa *MazeArena) Block(wc coords.World) Block {
 
 func (fa *MazeArena) Chunk(cc coords.Chunk) Chunk {
 	return GenerateChunk(fa, cc)
+}
+
+func (fa *MazeArena) Spawn() coords.World {
+	numSpawns := len(fa.spawnPointKeys)
+	if numSpawns > 0 {
+		// Get a random key value
+		key := fa.spawnPointKeys[rand.Intn(numSpawns)]
+		return fa.spawnPoints[key]
+	}
+	return coords.World {
+		X: 0,
+		Y: 21,
+		Z: 0,
+	}
 }
