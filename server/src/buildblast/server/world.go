e088faec3561751b369a761bac4b79acac6dc9b7
Kevin Nash <kieve10@hotmail.com>
2013-06-24 00:24:55 -0400

    Reintroduce log for chunks

--- server/src/buildblast/server/world.go
+++ server/src/buildblast/server/world.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"log"
 
 	"buildblast/physics"
 	"buildblast/mapgen"
@@ -49,6 +50,7 @@ func (w *World) RequestChunk(cc coords.Chunk) mapgen.Chunk {
 
 	if chunk == nil {
 		chunk = w.generator.Chunk(cc)
+		log.Println("Generated chunk at ", cc)
 
 		w.chunkLock.Lock()
 		w.chunks[cc] = chunk
