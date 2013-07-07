0e236262a1fa2895d0db24bda9cd6eb14fc84e9f
crazy2be <crazy1be@gmail.com>
2013-07-07 16:31:57 -0400

    Seperate Chunks and Blocks into seperate files.

--- server/src/buildblast/mapgen/chunk.go
+++ server/src/buildblast/mapgen/chunk.go
@@ -6,36 +6,30 @@ import (
 	"buildblast/coords"
 )
 
-type Block byte
-
-const (
-	BLOCK_NIL   = Block(0)
-	BLOCK_AIR   = Block(1)
-	BLOCK_DIRT  = Block(2)
-	BLOCK_STONE = Block(3)
-
-	// See "Block encoding.txt"
-
-	// Properties
-	BLOCK_MINEABLE  = 0x80000000
-
-	// Subtypes
-	BLOCK_EMPTY = 0x1 << 0
-	BLOCK_SOLID = 0x1 << 1
-)
-
-var BLOCK_PROPERTIES []uint32 = []uint32 {
-	/** NIL    */ 0,
-	/** AIR    */ BLOCK_EMPTY,
-	/** DIRT   */ BLOCK_SOLID | BLOCK_MINEABLE,
-	/** STONE  */ BLOCK_SOLID | BLOCK_MINEABLE,
-}
-
-func (b Block) Solid() bool {
-	return BLOCK_PROPERTIES[b] & BLOCK_SOLID > 0;
-}
-
 type Chunk [][][]Block
+
+func generateChunk(bg BlockSource, cc coords.Chunk) Chunk {
+	cw := coords.ChunkWidth
+	ch := coords.ChunkHeight
+	cd := coords.ChunkDepth
+
+	blocks := make([][][]Block, cw)
+	for ox := 0; ox < cw; ox++ {
+		blocks[ox] = make([][]Block, ch)
+		for oy := 0; oy < ch; oy++ {
+			blocks[ox][oy] = make([]Block, cd)
+			for oz := 0; oz < cd; oz++ {
+				bc := coords.Block{
+					X: ox + cc.X*cw,
+					Y: oy + cc.Y*ch,
+					Z: oz + cc.Z*cd,
+				}
+				blocks[ox][oy][oz] = bg.Block(bc)
+			}
+		}
+	}
+	return blocks
+}
 
 func (c Chunk) Block(oc coords.Offset) Block {
 	return c[oc.X][oc.Y][oc.Z]
--- server/src/buildblast/mapgen/main.go
+++ server/src/buildblast/mapgen/main.go
@@ -13,25 +13,3 @@ type BlockSource interface {
 	Block(bc coords.Block) Block
 }
 
-func GenerateChunk(bg BlockSource, cc coords.Chunk) Chunk {
-	cw := coords.ChunkWidth
-	ch := coords.ChunkHeight
-	cd := coords.ChunkDepth
-
-	blocks := make([][][]Block, cw)
-	for ox := 0; ox < cw; ox++ {
-		blocks[ox] = make([][]Block, ch)
-		for oy := 0; oy < ch; oy++ {
-			blocks[ox][oy] = make([]Block, cd)
-			for oz := 0; oz < cd; oz++ {
-				bc := coords.Block{
-					X: ox + cc.X*cw,
-					Y: oy + cc.Y*ch,
-					Z: oz + cc.Z*cd,
-				}
-				blocks[ox][oy][oz] = bg.Block(bc)
-			}
-		}
-	}
-	return blocks
-}
