c7d47b5fb4b131d104b1d5cb8735addbaa04106a
crazy2be <crazy1be@gmail.com>
2013-07-15 18:11:31 -0400

    Don't profile, fix spaces in pwd (who does that?)

--- server/src/buildblast/server/main.go
+++ server/src/buildblast/server/main.go
@@ -110,7 +110,7 @@ func main() {
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
 	go globalGame.Run()
-	go doProfile()
+// 	go doProfile()
 
 	http.HandleFunc("/", handler)
 	http.Handle("/sockets/main/", websocket.Handler(mainSocketHandler))
