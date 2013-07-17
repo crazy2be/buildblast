1d0f1f2000809ca617981b19f1f47468ad9a8e72
Quentin Brooks <yeerkkiller1@gmail.com>
2013-07-17 01:40:36 -0400

    Deleting some files.

--- client/js/perf/defaultSettings.js
+++
@@ -1,9 +0,0 @@
-//Global settings, try to keep the variables only 1 level deep, don't nest stuff.
-
-var settings = {};
-
-//To make it easy to find settings all settings should be in here, but set to false.
-//A locally stored localSettings.js file sets the settings too
-
-//Makes mining destroy a large portion of the map, making it more varied for testing purposes.
-settings.destroyMapOnMine = false;
