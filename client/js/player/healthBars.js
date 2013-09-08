fc6dc57f55faaa7ebffdf6eaa73f44633cff97e3
Quentin Brooks <yeerkkiller1@gmail.com>
2013-09-07 23:24:37 -0400

    Removed unused code.

--- client/js/player/healthBars.js
+++ client/js/player/healthBars.js
@@ -74,118 +74,11 @@ function HealthBars(world, camera, conn, controls) {
 		rotVec.x = -info.pos.x + camera.position.x;
 		rotVec.y = -info.pos.z + camera.position.z;
 
+		//I fell like this is wrong is some manner... not sure why though.
 		var dirRadian = Math.atan2(rotVec.x, rotVec.y);
-
 		mesh1.rotation.y = dirRadian;
 		//mesh1.rotation.x = 0.5;//Math.sin(dirRadian);
 		return mesh1;
-
-
-		function copy(src, dst) {
-			for (var i = 0; i < src.length; i++) {
-				dst[i] = src[i];
-			}
-		}
-
-		//2D rotation:
-		function setRotationAroundOrigin(point, radian) {
-			var curRotation = Math.atan2(point.x, point.y);
-			var newRotation = curRotation + radian;
-
-			var xFactor = Math.cos(newRotation);
-			var yFactor = Math.sin(newRotation);
-
-			var curMag = Math.sqrt(point.x * point.x + point.y * point.y);
-
-			point.x = curMag * xFactor;
-			point.y = curMag * yFactor;
-		}
-
-		var verts = [];
-		verts.push(-1, -1, 0);
-		verts.push(+1, -1, 0);
-		verts.push(+1, +1, 0);
-
-		verts.push(+1, +1, 0);
-		verts.push(-1, +1, 0);
-		verts.push(-1, -1, 0);
-
-		//var cameraDirectionVec = camera.rotation.clone();
-		//Not pointing where the camera is pointing, but pointing
-		//to the actual camera!
-		var rotVec = {};
-		rotVec.x = -info.pos.x + camera.position.x;
-		rotVec.y = -info.pos.z + camera.position.z;
-
-		var dirRadian = Math.atan2(rotVec.y, rotVec.x);
-
-		if(throttle++ % 1001 == 0) {
-			console.log(dirRadian);
-		}
-		for(var ix = 0; ix < verts.length; ix += 3){
-			var p = {x: verts[ix], y: verts[ix + 2]};
-			setRotationAroundOrigin(p, dirRadian);
-			verts[ix] = p.x;
-			verts[ix + 2] = p.y;
-
-			verts[ix] += info.pos.x;
-			verts[ix + 1] += info.pos.y;
-			verts[ix + 2] += info.pos.z;
-		}
-
-		//verts.push(-100, -100, -100);
-		//verts.push(100, 100, 100);
-		//verts.push(100, 100, -100);
-
-		var color = [];
-		for(var ix = 0; ix < verts.length; ix++) {
-			color[ix] =  (1 + ix) * 17 % 19 / 19;
-		}
-
-		var index = [];
-		for(var i = 0; i < verts.length; i += 9) {
-			var ix = i / 3;
-			index.push(ix, ix + 1, ix + 2);
-			index.push(ix + 2, ix + 1, ix);
-		}
-
-		var vertsa = new Float32Array(verts.length);
-		var colora = new Float32Array(color.length);
-		var indexa = new Uint16Array(index.length);
-		copy(verts, vertsa);
-		copy(color, colora);
-		copy(index, indexa);
-
-		var geometry = new THREE.BufferGeometry();
-
-		geometry.offsets = [{
-			start: 0,
-			count: indexa.length,
-			index: 0
-		}];
-		geometry.attributes = {
-			position: {
-				itemSize: 3, 
-				array: vertsa,
-				numItems: vertsa.length,
-				needsUpdate: true
-			},
-			color: {
-				itemSize: 3,
-				array: colora,
-				numItems: colora.length,
-				needsUpdate: true
-			},
-			index: {
-				itemSize: 1,
-				array: indexa,
-				numItems: indexa.length,
-				needsUpdate: true
-			}
-		};
-		var mesh = new THREE.Mesh(geometry, BAR_MATERIAL);
-
-		return mesh;
 	}
 
 	self.update = function (playerPosition, controlState) {
