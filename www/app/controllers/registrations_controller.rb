b9c06841d85e8c69e6575ec62facd05db3fd2117
Kieve <kieve10@hotmail.com>
2013-09-23 06:28:49 +0000

    You can now register, but it doesn't redirect and gives you bogus errors.

--- www/app/controllers/registrations_controller.rb
+++ www/app/controllers/registrations_controller.rb
@@ -1,10 +1,19 @@
-class UserController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+  def new
+    super
+  end
+
   def create
     @user = User.create(user_params)
+    super
+  end
+
+  def update
+    super
   end
 
   private
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation, :remember_me)
     end
-end
+end 
--- www/config/routes.rb
+++ www/config/routes.rb
@@ -5,7 +5,7 @@ Www::Application.routes.draw do
   unauthenticated do
     root :to => "home#index"
   end
-  devise_for :users
+  devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
