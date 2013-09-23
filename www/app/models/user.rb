1f0601d381ce01a0a8e8b5cf9c727c499f7bd493
Kieve <kieve10@hotmail.com>
2013-09-23 11:59:28 +0000

    Add email confirmation

--- www/app/models/user.rb
+++ www/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
   validates_presence_of :name
--- www/db/migrate/20130923054425_add_confirmed_at_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddConfirmedAtToUsers < ActiveRecord::Migration
-  def change
-    add_column :users, :confirmed_at, :time
-  end
-end
