1d9c21ef44d68028d398d3e569fc4d883cb59069
Kieve <kieve10@hotmail.com>
2013-09-23 05:32:16 +0000

    Add the home page (server still in a good state)

--- www/config/routes.rb
+++ www/config/routes.rb
@@ -1,4 +1,11 @@
 Www::Application.routes.draw do
+  get "home/index"
+  authenticated :user do
+    root :to => 'home#index', as: :authenticated_root
+  end
+  unauthenticated do
+    root :to => "home#index"
+  end
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
