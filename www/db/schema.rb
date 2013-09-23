872d3bb150f8fa34f5ba5acb1e00faea056aa439
Kieve <kieve10@hotmail.com>
2013-09-23 05:44:52 +0000

    Create the first user!

--- www/db/schema.rb
+++ www/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130923051338) do
+ActiveRecord::Schema.define(version: 20130923054425) do
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20130923051338) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "name"
+    t.time     "confirmed_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
--- www/spec/factories/users.rb
+++ www/spec/factories/users.rb
@@ -5,6 +5,6 @@ FactoryGirl.define do
     password 'changeme'
     password_confirmation 'changeme'
     # required if the Devise Confirmable module is used
-    # confirmed_at Time.now
+    confirmed_at Time.now
   end
 end
