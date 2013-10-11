9324c669c394d4c49e939eca13792ecc95b260ef
crazy2be <crazy1be@gmail.com>
2013-10-10 22:14:27 -0700

    Fix indentation.

--- client/js/featureTester.js
+++ client/js/featureTester.js
@@ -47,12 +47,12 @@ return function FeatureTester() {
 		if (!tests.webgl) {
 			var elm = document.createElement('p');
 			elm.innerHTML = window.WebGLRenderingContext ? [
-			'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
-			'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-		].join('\n') : [
-			'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
-			'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
-		].join('\n');
+				'Your graphics card does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br />',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join('\n') : [
+				'Your browser does not seem to support <a href="http://khronos.org/webgl/wiki/Getting_a_WebGL_Implementation" style="color:#000">WebGL</a>.<br/>',
+				'Find out how to get it <a href="http://get.webgl.org/" style="color:#000">here</a>.'
+			].join('\n');
 			errors.appendChild(elm);
 		}
 		if (!tests.workers) {
