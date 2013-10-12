c9eaed9505334288a584f2e1e5559651ccc0c0ec
crazy2be <crazy1be@gmail.com>
2013-10-12 15:38:13 -0700

    Don't indent so much!

--- client/js/entities/playerEntity.js
+++ client/js/entities/playerEntity.js
@@ -1,77 +1,78 @@
 define(function (require) {
-	var THREE = require("THREE");
-	var PLAYER = require("player/playerSize");
-	var EntityState = require("./entityState");
 
-	return function PlayerEntity() {
-		var self = this;
+var THREE = require("THREE");
+var PLAYER = require("player/playerSize");
+var EntityState = require("./entityState");
 
-		self.pos = function () {
-			return state.pos;
-		};
+return function PlayerEntity() {
+	var self = this;
 
-		self.look = function () {
-			return state.look;
-		};
+	self.pos = function () {
+		return state.pos;
+	};
 
-		self.health = function () {
-			return state.health;
-		};
+	self.look = function () {
+		return state.look;
+	};
 
-		self.vy = function () {
-			return state.vy;
-		};
+	self.health = function () {
+		return state.health;
+	};
 
-		self.contains = function (x, y, z) {
-			var box = new Box(pos, PLAYER.HALF_EXTENTS, PLAYER.CENTER_OFFSET);
-			return box.contains(x, y, z);
-		};
+	self.vy = function () {
+		return state.vy;
+	};
 
-		self.addTo = function (scene) {
-			scene.add(entityMesh);
-		};
+	self.contains = function (x, y, z) {
+		var box = new Box(pos, PLAYER.HALF_EXTENTS, PLAYER.CENTER_OFFSET);
+		return box.contains(x, y, z);
+	};
 
-		self.removeFrom = function (scene) {
-			scene.remove(entityMesh);
-		};
+	self.addTo = function (scene) {
+		scene.add(entityMesh);
+	};
 
-		var uIViews = [];
-		self.add = function (view) {
-			uIViews.push(view);
-			view.meshes().forEach(entityMesh.add.bind(entityMesh));
-		}
+	self.removeFrom = function (scene) {
+		scene.remove(entityMesh);
+	};
 
-		var state = new EntityState();
+	var uIViews = [];
+	self.add = function (view) {
+		uIViews.push(view);
+		view.meshes().forEach(entityMesh.add.bind(entityMesh));
+	}
 
-		var entityMesh = new THREE.Object3D();
+	var state = new EntityState();
 
-		self.update = function (newState, clock) {
-			state = newState;
+	var entityMesh = new THREE.Object3D();
 
-			var pos = self.pos();
-			var look = self.look();
+	self.update = function (newState, clock) {
+		state = newState;
 
-			var co = PLAYER.CENTER_OFFSET;
-			var c = new THREE.Vector3(
-				pos.x + co.x,
-				pos.y + co.y,
-				pos.z + co.z
+		var pos = self.pos();
+		var look = self.look();
+
+		var co = PLAYER.CENTER_OFFSET;
+		var c = new THREE.Vector3(
+			pos.x + co.x,
+			pos.y + co.y,
+			pos.z + co.z
+		);
+		entityMesh.position.set(c.x, c.y, c.z);
+
+		function lookAt(obj, pos, x, y, z) {
+			var target = new THREE.Vector3(
+				pos.x + x,
+				pos.y + y,
+				pos.z + z
 			);
-			entityMesh.position.set(c.x, c.y, c.z);
+			obj.lookAt(target);
+		}
+		lookAt(entityMesh, c, look.x, 0, look.z);
 
-			function lookAt(obj, pos, x, y, z) {
-				var target = new THREE.Vector3(
-					pos.x + x,
-					pos.y + y,
-					pos.z + z
-				);
-				obj.lookAt(target);
-			}
-			lookAt(entityMesh, c, look.x, 0, look.z);
-
-			for (var i = 0; i < uIViews.length; i++) {
-				uIViews[i].update(self, clock);
-			}
+		for (var i = 0; i < uIViews.length; i++) {
+			uIViews[i].update(self, clock);
 		}
 	}
+}
 });
