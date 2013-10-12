ca6cba0dc9a5be8cc28331f6286156fab6428c4a
crazy2be <crazy1be@gmail.com>
2013-10-12 14:15:46 -0700

    Fix indentation.

--- client/js/math.js
+++ client/js/math.js
@@ -27,9 +27,10 @@ return function addToContext(context) {
 	context.signum = function (n) {
 		return n < 0 ? -1 : n > 0 ? 1 : 0;
 	}
-
+	// You only need this on the worker thread,
+	//
 	context.dist = function (p1, p2) {
-		return Math.sqrt(Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2) + Math.pow(p1.z - p2.z, 2));
+		return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2) + pow(p1.z - p2.z, 2));
 	}
 }
 });
