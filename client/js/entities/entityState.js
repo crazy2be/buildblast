0bad1f8e49f66cb2c2cd214cd88424f9f8991fdd
yeerkkiller1 <yeerkkiller1@gmail.com>
2013-10-13 20:13:47 -0400

    VS likes to reformat stuff.

--- client/js/entities/entityState.js
+++ client/js/entities/entityState.js
@@ -7,17 +7,17 @@ return function EntityState(pos, look, health, vy) {
 	this.vy = vy || 0.0;
 	this.clone = function () {
 		var cloned = new EntityState(
-		this.pos.clone(),
-		this.look.clone(),
-		this.health,
-		this.vy);
+			this.pos.clone(),
+			this.look.clone(),
+			this.health,
+			this.vy);
 		return cloned;
 	};
 	this.lerp = function (other, frac) {
 		this.pos.lerp(other.pos, frac);
 		this.look.lerp(other.look, frac);
-		this.health = this.health * (1 - frac) + other.health * frac;
-		this.vy = this.vy * (1 - frac) + other.vy * frac;
+		this.health = this.health*(1 - frac) + other.health*frac;
+		this.vy = this.vy*(1 - frac) + other.vy*frac;
 		return this;
 	}
 }
