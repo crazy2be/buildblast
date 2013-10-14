3f6a9826b1200e7220e5470799bafdba153eafdb
yeerkkiller1 <yeerkkiller1@gmail.com>
2013-10-13 20:15:41 -0400

    If only github diff ignored whitespace.

--- client/js/settings.js
+++ client/js/settings.js
@@ -3,29 +3,29 @@ define(function () {
 // at your leasure in the javascript console.
 function __loadSettingsScheme(name) {
 	switch (name) {
-		case "quentin":
-			//Clear the existing scheme... not sure if anyone else wants this?
-			__loadSettingsScheme();
-			localStorage.lagInductionTime = 300;
-			localStorage.thirdPerson = true;
-			localStorage.showHistoryBuffers = true;
-			localStorage.hpBars = true;
-			return "Success!";
-		case "justin":
-			localStorage.useDvorak = true;
-			localStorage.mouseMoveBug = true;
-			return "Success!";
-		case undefined:
-		case "default":
-			for (var k in localStorage) {
-				// Can't use = false, because it gets
-				// serialized as "false" in localStorage,
-				// which is thruthy to JavaScript.
-				delete localStorage[k];
-			}
-			return "Success!";
-		default:
-			return "No clue what the settings scheme: '" + name + "' means?";
+	case "quentin":
+		//Clear the existing scheme... not sure if anyone else wants this?
+		__loadSettingsScheme();
+		localStorage.lagInductionTime = 300;
+		localStorage.thirdPerson = true;
+		localStorage.showHistoryBuffers = true;
+		localStorage.hpBars = true;
+		return "Success!";
+	case "justin":
+		localStorage.useDvorak = true;
+		localStorage.mouseMoveBug = true;
+		return "Success!";
+	case undefined:
+	case "default":
+		for (var k in localStorage) {
+			// Can't use = false, because it gets
+			// serialized as "false" in localStorage,
+			// which is thruthy to JavaScript.
+			delete localStorage[k];
+		}
+		return "Success!";
+	default:
+		return "No clue what the settings scheme: '" + name + "' means?";
 	}
 }
 return __loadSettingsScheme;
