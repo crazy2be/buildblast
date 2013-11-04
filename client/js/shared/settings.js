3bb4d7a552e0f770ec4b2ed57153f95a2e2b551d
yeerkkiller1 <yeerkkiller1@gmail.com>
2013-11-03 23:31:57 -0500

    Stuff for justin.

--- client/js/shared/settings.js
+++ client/js/shared/settings.js
@@ -1,34 +1,35 @@
-//Yes I made this file, and no I am not removing it.
-//	Our settings will likely always be stored entirely
-//	in local storage, so the way we load settings is
-//	appropriate. Named settings are appropriate too,
-//	as there is no point in having a scheme which no
-//	ones uses, and so the only viable schemes currently
-//	are the ones exactly as we want them (so ones that
-//	exit just for us).
+define(["core/string"], function (string) {
 
-define([], function () {
-	return {
-		loadSettings: loadSettings
+// Conventient bundles of settings which you can activate
+// at your leasure in the javascript console.
+function __loadSettingsScheme(name) {
+	if (string.startsWith(name, '!')) {
+		name = string.trimStart(name, '!');
+		__loadSettingsScheme("default");
 	}
 
-	function loadSettings() {
-		if (!localStorage.scheme) return;
-
-		switch (localStorage.scheme) {
-			case "quentin": 
-				{
-					localStorage.viewsVisible = true;
-					localStorage.lag = 300;
-					localStorage.thirdPerson = true;
-					localStorage.posHistoryBar = true;
-					break;
-				}
-			default: 
-				{
-					console.warn("No clue what the settings scheme: '" + localStorage.scheme + "' means?")
-					break;
-				}
+	switch (name) {
+	case "quentin":
+		localStorage.lagInductionTime = 300;
+		localStorage.thirdPerson = true;
+		localStorage.showHistoryBuffers = true;
+		localStorage.hpBars = true;
+		return "Success!";
+	case "justin":
+		localStorage.useDvorak = true;
+		localStorage.mouseMoveBug = true;
+		return "Success!";
+	case "default":
+		for (var k in localStorage) {
+			// Can't use = false, because it gets
+			// serialized as "false" in localStorage,
+			// which is thruthy to JavaScript.
+			delete localStorage[k];
 		}
+		return "Success!";
+	default:
+		return "No clue what the settings scheme: '" + name + "' means?";
 	}
+}
+return __loadSettingsScheme;
 });
