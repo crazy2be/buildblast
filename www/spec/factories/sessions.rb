e3ddedbb2de4161977dea525346da89e7e45eff6
Kieve <kieve10@hotmail.com>
2014-01-16 14:30:19 +0000

    Automated testing is overrated.

--- www/spec/factories/sessions.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :session do
-    user_id 1
-    session_id "MyString"
-    session_cookie "MyString"
-  end
-end
