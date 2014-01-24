070b74f22b152daf2dd9e5156f47322d99e2ecd4
Kevin Nash <kieve10@hotmail.com>
2014-01-23 22:04:46 -0500

    Implement first version of server manager

--- server-manager/main.go
+++ server-manager/main.go
@@ -1,9 +1,73 @@
-package main;
+package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
+	"os/exec"
+	"runtime"
+	"strconv"
+	"sync"
 )
 
+var BASE_PORT = 10000
+
+var globalServerMap = NewServerMap()
+
+type ServerMap struct {
+	servers map[int]*exec.Cmd
+	mutex   sync.Mutex
+}
+
+func (sm *ServerMap) Put(id int, handle *exec.Cmd) {
+	sm.mutex.Lock()
+	sm.servers[id] = handle
+	sm.mutex.Unlock()
+}
+
+func NewServerMap() *ServerMap {
+	return &ServerMap{
+		servers: make(map[int]*exec.Cmd),
+	}
+}
+
+type ApiCreateServer struct {
+	Id int `json:",string"`
+}
+
+func runServer(id int) {
+	app := "./server"
+
+	arg0 := "-client"
+	arg1 := "client"
+	arg2 := "-host"
+	arg3 := ":" + strconv.Itoa(BASE_PORT+id)
+
+	cmd := exec.Command(app, arg0, arg1, arg2, arg3)
+	globalServerMap.Put(id, cmd)
+	err := cmd.Run()
+
+	if err != nil {
+		log.Println(err.Error())
+	}
+}
+
+func startServerHandler(w http.ResponseWriter, r *http.Request) {
+	var server ApiCreateServer
+	err := json.NewDecoder(r.Body).Decode(&server)
+	if err != nil {
+		log.Println("Error when starting server.", err)
+	}
+
+	go runServer(server.Id)
+}
+
 func main() {
-	log.Println("Hello world!")
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	http.HandleFunc("/", startServerHandler)
+
+	err := http.ListenAndServe(":3001", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
 }
--- server/authentication.go
+++ server/authentication.go
@@ -4,14 +4,13 @@ import (
 	"crypto/tls"
 	"encoding/json"
 	"errors"
-	"io/ioutil"
 	"net/http"
 
 	"code.google.com/p/go.net/websocket"
 )
 
 type ApiUserResponse struct {
-	Id        int
+	Id        int `json:",string"`
 	Email     string
 	Name      string
 	CreatedAt string `json:"created_at"`
@@ -51,14 +50,8 @@ func Authenticate(ws *websocket.Conn) (*ApiUserResponse, *AuthError) {
 		return nil, &AuthError{Message: "Auth server connection issue.", Err: err}
 	}
 
-	// Parse the response
-	defer res.Body.Close()
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, &AuthError{Message: "Could not read auth response.", Err: err}
-	}
 	var data ApiUserResponse
-	err = json.Unmarshal(body, &data)
+	err = json.NewDecoder(res.Body).Decode(&data)
 	if err != nil {
 		return nil, &AuthError{Message: "Could not parse auth response.", Err: err}
 	}
