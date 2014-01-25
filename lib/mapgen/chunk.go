8124e554aeeefeb2c34f3a3be25cf4cb3fd1ab1b
crazy2be <code@jmcgirr.com>
2014-01-25 17:19:11 -0500

    Make chunks allocated flat.
    More efficient memory usage, probably faster accesses, and less GC pressure.

--- lib/mapgen/chunk.go
+++ lib/mapgen/chunk.go
@@ -6,7 +6,12 @@ import (
 	"buildblast/lib/coords"
 )
 
-type Chunk [][][]Block
+type Chunk []Block
+const blocksPerChunk = coords.ChunkWidth * coords.ChunkHeight * coords.ChunkDepth
+
+func allocChunk() (Chunk) {
+	return make([]Block, blocksPerChunk)
+}
 
 func generateChunk(bg blockGenerator, cc coords.Chunk) (Chunk, []coords.World) {
 	cw := coords.ChunkWidth
@@ -14,11 +19,9 @@ func generateChunk(bg blockGenerator, cc coords.Chunk) (Chunk, []coords.World) {
 	cd := coords.ChunkDepth
 
 	spawns := make([]coords.World, 0)
-	blocks := make([][][]Block, cw)
+	chunk := allocChunk();
 	for ocX := 0; ocX < cw; ocX++ {
-		blocks[ocX] = make([][]Block, ch)
 		for ocY := 0; ocY < ch; ocY++ {
-			blocks[ocX][ocY] = make([]Block, cd)
 			for ocZ := 0; ocZ < cd; ocZ++ {
 				bc := coords.Block{
 					X: ocX + cc.X*cw,
@@ -26,22 +29,26 @@ func generateChunk(bg blockGenerator, cc coords.Chunk) (Chunk, []coords.World) {
 					Z: ocZ + cc.Z*cd,
 				}
 				block, isSpawn := bg.Block(bc)
-				blocks[ocX][ocY][ocZ] = block
+				chunk.SetBlock(bc.Offset(), block)
 				if isSpawn {
 					spawns = append(spawns, bc.Center())
 				}
 			}
 		}
 	}
-	return blocks, spawns
+	return chunk, spawns
 }
 
 func (c Chunk) Block(oc coords.Offset) Block {
-	return c[oc.X][oc.Y][oc.Z]
+	cw := coords.ChunkWidth
+	ch := coords.ChunkHeight
+	return c[oc.X*cw*ch + oc.Y*cw + oc.Z]
 }
 
 func (c Chunk) SetBlock(oc coords.Offset, newBlock Block) {
-	c[oc.X][oc.Y][oc.Z] = newBlock
+	cw := coords.ChunkWidth
+	ch := coords.ChunkHeight
+	c[oc.X*cw*ch + oc.Y*cw + oc.Z] = newBlock
 }
 
 // Flatten returns the chunk data as a string. It
@@ -57,42 +64,23 @@ func (c Chunk) SetBlock(oc coords.Offset, newBlock Block) {
 //     isn't particulilly fast at serializing large
 //     arrays of numbers.
 func (c Chunk) Flatten() string {
-	cw := coords.ChunkWidth
-	ch := coords.ChunkHeight
-	cd := coords.ChunkDepth
-	data := make([]byte, cw*ch*cd)
-	for ocX := 0; ocX < cw; ocX++ {
-		for ocY := 0; ocY < ch; ocY++ {
-			for ocZ := 0; ocZ < cd; ocZ++ {
-				// 35: # charater. Control charaters
-				// are not allowed in JSON strings, and
-				// we want to avoid '"', which requires
-				// escaping.
-				value := byte(c[ocX][ocY][ocZ] + 35)
-				if value >= 127 || value < 35 {
-					panic(fmt.Sprintf("Attempted to encode out of range value of '%d' to chunk data. (It might work but we need to test it)", value))
-				}
-				data[ocX*cw*ch+ocY*cw+ocZ] = value
-			}
+	data := make([]byte, blocksPerChunk)
+	for i := 0; i < blocksPerChunk; i++ {
+		// 35: # charater. Control charaters are not allowed in JSON
+		// strings, and we want to avoid '"', which requires escaping.
+		value := byte(c[i] + 35)
+		if value >= 127 || value < 35 {
+			panic(fmt.Sprintf("Attempted to encode out of range value of '%d' to chunk data. (It might work but we need to test it)", value))
 		}
+		data[i] = value
 	}
 	return string(data)
 }
 
 func (c Chunk) Clone() Chunk {
-	cw := coords.ChunkWidth
-	ch := coords.ChunkHeight
-	cd := coords.ChunkDepth
-
-	blocks := make([][][]Block, cw)
-	for ocX := 0; ocX < cw; ocX++ {
-		blocks[ocX] = make([][]Block, ch)
-		for ocY := 0; ocY < ch; ocY++ {
-			blocks[ocX][ocY] = make([]Block, cd)
-			for ocZ := 0; ocZ < cd; ocZ++ {
-				blocks[ocX][ocY][ocZ] = c[ocX][ocY][ocZ]
-			}
-		}
+	newChunk := allocChunk()
+	for i := 0; i < blocksPerChunk; i++ {
+		newChunk[i] = c[i];
 	}
-	return blocks
+	return newChunk
 }
