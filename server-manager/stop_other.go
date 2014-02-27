1426a899eeea9d44537765bd2ed530d003a8d3b3
Kieve <kieve10@hotmail.com>
2014-02-27 10:21:32 -0500

    Add missing error return

--- server-manager/stop_other.go
+++ server-manager/stop_other.go
@@ -11,6 +11,7 @@ func stop(process *os.Process) error {
 	err := process.Signal(os.Interrupt)
 	if err != nil {
 		log.Println("Error while sending SIGINT to running server:", err)
+		return err
 	}
 	_, err = process.Wait()
 	return err
