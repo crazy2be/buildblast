62505b83e27b7c2e6b821e69a54ca84df77d40ee
Kieve <kieve10@hotmail.com>
2014-04-25 09:37:09 -0400

    Fix rounding artifacts at the edges

--- client/js/chunks/worker/meshers/simpleMesher.js
+++ client/js/chunks/worker/meshers/simpleMesher.js
@@ -172,6 +172,9 @@ return function simpleMesh(blocks, cc, manager) {
 			var v = 1 - Block.UV_UNIT - (tileOffset[1] * Block.UV_UNIT);
 			u += uvWind[0] * Block.UV_UNIT;
 			v += uvWind[1] * Block.UV_UNIT;
+			// Add a 12.5% texel inset at the edges, to prevent rounding artifacts.
+			u += (uvWind[0] === 1 ? -1 : 1) / (Block.ATLAS_SIZE * 8);
+			v += (uvWind[1] === 1 ? -1 : 1) / (Block.ATLAS_SIZE * 8);
 			uvs.push(u, v);
 		}
 	}
