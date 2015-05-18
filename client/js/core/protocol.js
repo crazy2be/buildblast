b38b06a4dd6f07b686fe890ea03ef1d28b3fb9ab
kieve <kieve10@hotmail.com>
2015-05-18 09:46:55 -0400

    Change the client protocol structure to mirror the Go structure (of appending and returning buffers). Implement the String marshalling. Need to test string encoding (which will regression test my varint encoding, since the varints are used in the strings).

--- client/js/core/protocol.js
+++ client/js/core/protocol.js
@@ -7,26 +7,34 @@ var BioticState = require("entities/model/bioticState");
 var WorldItemState = require("entities/model/worldItemState");
 
 var Protocol = {
-	MSG_HANDSHAKE_REPLY: 0,
-	MSG_HANDSHAKE_ERROR: 1,
-	MSG_ENTITY_CREATE: 2,
-	MSG_ENTITY_STATE: 3,
-	MSG_ENTITY_REMOVE: 4,
-	MSG_CHUNK: 5,
-	MSG_BLOCK: 6,
-	MSG_CONTROLS_STATE: 7,
-	MSG_CHAT: 8,
-	MSG_DEBUG_RAY: 9,
-	MSG_NTP_SYNC: 10,
-	MSG_INVENTORY_STATE: 11,
-	MSG_INVENTORY_MOVE: 12,
-	MSG_SCOREBOARD_ADD: 13,
-	MSG_SCOREBOARD_SET: 14,
+	MSG_HANDSHAKE_REPLY:    0,
+	MSG_HANDSHAKE_ERROR:    1,
+	MSG_ENTITY_CREATE:      2,
+	MSG_ENTITY_STATE:       3,
+	MSG_ENTITY_REMOVE:      4,
+	MSG_CHUNK:              5,
+	MSG_BLOCK:              6,
+	MSG_CONTROLS_STATE:     7,
+	MSG_CHAT:               8,
+	MSG_DEBUG_RAY:          9,
+	MSG_NTP_SYNC:          10,
+	MSG_INVENTORY_STATE:   11,
+	MSG_INVENTORY_MOVE:    12,
+	MSG_SCOREBOARD_ADD:    13,
+	MSG_SCOREBOARD_SET:    14,
 	MSG_SCOREBOARD_REMOVE: 15
 };
 
+Protocol.append = function(a, b) {
+	var result = new Uint8Array(a.byteLength + b.byteLength);
+	result.set(new Uint8Array(a), 0);
+	result.set(new Uint8Array(b), a.byteLength);
+	return result.buffer;
+};
+
 // Ported from varint.go
-Protocol.marshalInt = function(offset, dataView, x) {
+Protocol.marshalInt = function(x) {
+	var buf = new Uint8Array(10);
 	var ux = (x << 1) >>> 0;
 	if (x < 0) {
 		ux = ~ux;
@@ -34,12 +42,12 @@ Protocol.marshalInt = function(offset, dataView, x) {
 
 	var i = 0;
 	while (ux >= 0x80) {
-		dataView.setUint8(offset + i, ux | 0x80);
+		buf[i] = ux | 0x80;
 		ux >>>= 7;
 		i++;
 	}
-	dataView.setUint8(offset + i, ux);
-	return i+1;
+	buf[i] = ux;
+	return buf.subarray(0, i).buffer;
 };
 
 // Ported from varint.go
@@ -68,17 +76,37 @@ Protocol.unmarshalInt = function(offset, dataView) {
 	}
 };
 
-Protocol.marshalFloat64 = function(offset, dataView, x) {
-	dataView.setFloat64(offset, x);
-	return 8;
+Protocol.marshalFloat64 = function(x) {
+	var buf = new ArrayBuffer(8);
+	var dataView = new DataView(buf);
+	dataView.setFloat64(x);
+	return buf;
 };
 
 Protocol.unmarshalFloat64 = function(offset, dataView) {
 	return { value: dataView.getFloat64(offset), read: 8 }
 };
 
-Protocol.marshalString = function(offset, dataView, s) {
+Protocol.marshalString = function(s) {
+	var result = Protocol.marshalInt(s.length*2);
 
+	var buf = new ArrayBuffer(s.length*2); // 2 bytes for each char
+	var bufView = new Uint16Array(buf);
+	for (var i = 0, strLen=str.length; i < strLen; i++) {
+		bufView[i] = s.charCodeAt(i);
+	}
+	return Protocol.append(result, buf);
+};
+
+Protocol.unmarshalString = function(offset, dataView) {
+	function uintToString(uintArray) {
+		var encodedString = String.fromCharCode.apply(null, uintArray);
+		return decodeURIComponent(escape(encodedString));
+	}
+	var returned = Protocol.unmarshalInt(0, dataView);
+	offset += returned.read;
+	return { value: uintToString(new Uint8Array(dataView.buffer.slice(offset, offset + returned.value))),
+			 read: returned.value + returned.read }
 };
 
 Protocol.parseBody = function(proto) {
